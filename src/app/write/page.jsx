a2d6f8405cf0df6731b8b4b5dcee0cf76c04ea85
tareqhasan382 <tareqhasan382@gmail.com>
2024-03-11 02:31:32 +0600

    protect route

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,11 +1,13 @@
 import Write from '@/components/Write';
 import React from 'react';
-import PrivateRoute from '../api/PrivateRoute';
+import PrivateRoute from '../../PrivateRoute';
 //className=" px-5 max-w-[1280px] h-auto mx-auto py-10 gap-5 overflow-x-hidden "
 const page = () => {
   return (
     
-    <Write/>
+    <PrivateRoute>
+      <Write/>
+    </PrivateRoute>
 
   )
  
