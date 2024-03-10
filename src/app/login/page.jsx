72bbb4b12c56e8db9a40b508cda75990595417a3
tareqhasan382 <tareqhasan382@gmail.com>
2024-03-11 02:26:07 +0600

    bug fix authentication

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,8 +5,8 @@ import { authOptions } from '../api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
 
 const page = async() => {
-  // const session = await getServerSession(authOptions)
-  // if(session)redirect("/")
+  const session = await getServerSession(authOptions)
+  if(session)redirect("/")
   return <LoginForm/>
   
 }
